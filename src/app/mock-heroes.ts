f2c99a6fa3ee157971a03da3a9875c089c67626b
Fahad Shaikh <fahad.shaikh@crowdlinker.com>
2018-04-10 01:06:35 -0400

    Intitial Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'V Kohli' },
+    { id: 2, name: 'H Pandya' },
+    { id: 3, name: 'S Raina' },
+    { id: 4, name: 'MS Dhoni' },
+    { id: 5, name: 'Y Singh' },
+    { id: 6, name: 'R Sharma' },
+    { id: 7, name: 'R Ashwin' },
+    { id: 8, name: 'G Gambhir' },
+    { id: 9, name: 'R Jadeja' },
+    { id: 10, name: 'M Shami' }
+];
